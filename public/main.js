eb928577a7c779194427e5dc72a97ab305699cd5
ikow-g <muh.djatmieka@gmail.com>
2024-05-14 12:54:04 +0700

    configure mix

---
+++ public/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "?":
+/*!*********!*\
+  !*** ? ***!
+  \*********/
+/***/ (() => {
+
+throw new Error("Module build failed: Error: Final loader (unknown) didn't return a Buffer or String\n    at processResult (C:\\app\\lxrmix\\node_modules\\webpack\\lib\\NormalModule.js:857:17)\n    at C:\\app\\lxrmix\\node_modules\\webpack\\lib\\NormalModule.js:966:5\n    at C:\\app\\lxrmix\\node_modules\\loader-runner\\lib\\LoaderRunner.js:407:3\n    at iterateNormalLoaders (C:\\app\\lxrmix\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:10)\n    at processResource (C:\\app\\lxrmix\\node_modules\\loader-runner\\lib\\LoaderRunner.js:227:3)\n    at iteratePitchingLoaders (C:\\app\\lxrmix\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\app\\lxrmix\\node_modules\\loader-runner\\lib\\LoaderRunner.js:398:2)\n    at NormalModule._doBuild (C:\\app\\lxrmix\\node_modules\\webpack\\lib\\NormalModule.js:905:3)\n    at NormalModule.build (C:\\app\\lxrmix\\node_modules\\webpack\\lib\\NormalModule.js:1081:15)\n    at C:\\app\\lxrmix\\node_modules\\webpack\\lib\\Compilation.js:1400:12\n    at NormalModule.needBuild (C:\\app\\lxrmix\\node_modules\\webpack\\lib\\NormalModule.js:1407:32)\n    at Compilation._buildModule (C:\\app\\lxrmix\\node_modules\\webpack\\lib\\Compilation.js:1381:10)\n    at C:\\app\\lxrmix\\node_modules\\webpack\\lib\\util\\AsyncQueue.js:305:10\n    at Hook.eval [as callAsync] (eval at create (C:\\app\\lxrmix\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\app\\lxrmix\\node_modules\\tapable\\lib\\Hook.js:18:14)\n    at AsyncQueue._startProcessing (C:\\app\\lxrmix\\node_modules\\webpack\\lib\\util\\AsyncQueue.js:295:26)\n    at AsyncQueue._ensureProcessing (C:\\app\\lxrmix\\node_modules\\webpack\\lib\\util\\AsyncQueue.js:282:12)\n    at process.processImmediate (node:internal/timers:476:21)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["?"]();
+/******/ 	
+/******/ })()
+;
